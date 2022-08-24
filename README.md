b1127e665aead6b5b058ca9b3c1947ef73f7b3c8
Gabriel de Campos <gabodecampos@gmail.com>
2022-08-24 02:41:34 -0400

    Finalização do teste em javascript

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # challenge-escribo-logic
+
+### JAVASCRIPT
+
+*Para roda o arquivo .js é necessário nodejs instalado.*
+
+Executar o comando:
+
+```shell
+$ node index.js
+```
---
+++ index.js
@@ -0,0 +1,46 @@
+const readline = require('node:readline');
+const { stdin: input, stdout: output } = require('node:process');
+
+const rl = readline.createInterface({ input, output });
+
+const groupByThree = []
+let sumGroupByThree = 0
+
+const groupByFive = []
+let sumGroupByFive = 0
+
+const divisibleByThree = (input) => {
+    return (input % 3) == 0 ? true : false
+}
+
+const divisibleByFive = (input) => {
+    return (input % 5) == 0 ? true : false
+}
+
+rl.question('Informe um numero inteiro: ', (input) => {
+
+  console.log(`\n`)
+
+  let baseCalc = parseInt(input)
+
+  for (let i = 0; i < baseCalc; i++) {
+    if (divisibleByThree(i)) {
+        groupByThree.push(i)
+        sumGroupByThree += i
+    }
+
+    if (divisibleByFive(i)) {
+        groupByFive.push(i)
+        sumGroupByFive += i
+    }
+  } 
+  
+  console.log(`Número divisiveis por 3: ${groupByThree}`)
+  console.log(`A soma dos numeros divisiveis por 3 é ${sumGroupByThree} \n`)
+
+  console.log(`Número divisiveis por 5: ${groupByFive}`)
+  console.log(`A soma dos numeros divisiveis por 5 é ${sumGroupByFive} \n`)
+
+  rl.close();
+});
+
